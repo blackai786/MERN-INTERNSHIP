910446dee825924bcd4e4a96c13004021d5485c6
blackai786 <sahilshaikh120381718@gmail.com>
2026-02-12 15:59:49 +0530

    Removed all files

--- src/component/FooterComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "../assets/css/footer.css"
-export const FooterComponent = () => {
-  return (
-    <div className='footer'>
-        <h2>ありがとう!!</h2>
-    </div>
-  )
-}
--- src/component/HeaderComponent1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "../assets/css/header.css"
-export const HeaderComponent1 = () => {
-  return (
-    <div className='header'>
-        <h1>Information</h1>
-    </div>
-  )
-}
